7fd245c113cf35e86f29abeea0100f04ef0966ec
Chris Erin <dev+chriserin@hashrocket.com>
2017-12-08 15:38:09 -0600

    Update index page

--- update_concepts.rb
+++ update_concepts.rb
@@ -128,7 +128,9 @@ concepts.each do |concept|
   concept_yaml = YAML.load(concept[:concept_config]['text'])
 
   banner_sub_filter = if concept_yaml['banner']
-                        "sub_filter '<body>' '<header>hashrocket</header>';"
+                        "sub_filter <body> '<body><header style="background-color: #414042; height: 2rem; padding: 0.4rem; color: white;"><img src="https://d15zqjc70bk603.cloudfront.net/assets/brand/hr_logo_h_light-4cb402f22041c39699a752bd21aaa38ebd860b343ae20a5fe97342c8ec53f156.svg"></img></header>';"
+
+
                       end
   concept[:concept_url] = "#{concept_yaml['name']}.hrcpt.online";
 
@@ -166,16 +168,86 @@ File.write("/var/www/concepts.com/index.html", ERB.new(DATA.read).result(binding
 __END__
 
 <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <link rel="shortcut icon" href="https://hashrocket.com/favicon.ico">
+    <title>Concepts - Hashrocket</title>
+    <styles>
+      header {
+        background-color: #414042;
+        height: 2rem;
+        padding: 0.4rem;
+        color: white;
+      }
+
+      body {
+        background-color: white;
+        font: Helvetica, sans-serif;
+      }
+
+      ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        min-width: 42rem;
+        max-width: 60rem;
+        height: 100%;
+      }
+
+      li {
+        display: flex;
+        flex-direction: column;
+        background-color: #c8c8c8;
+        min-width: 16rem;
+        min-height: 8rem;
+        margin: 2rem;
+      }
+
+      li h2 {
+        color: #af1e23;
+      }
+
+      li div {
+        min-height: 1.6rem;
+      }
+
+      a:link, a:active, a:visited, a:hover, a:focus
+      {
+        decoration: none;
+        color: #414042;
+      }
+
+    </styles>
+  </head>
   <body>
-    <ul>
-      <% concepts.each do |concept| %>
-      <% concept_yaml = YAML.load(concept[:concept_config]['text']) %>
-      <li>
-	<a href="http://<%= concept[:concept_url] %>">
-	<%= concept_yaml['name'] %>
-	</a>
-      </li>
-      <% end %>
-    </ul>
+    <header>
+      <img src="https://d15zqjc70bk603.cloudfront.net/assets/brand/hr_logo_h_light-4cb402f22041c39699a752bd21aaa38ebd860b343ae20a5fe97342c8ec53f156.svg" class="App-logo" alt="logo">
+    </header>
+    <section>
+      <h1>General</h1>
+      <ul>
+        <% concepts.each do |concept| %>
+        <% concept_yaml = YAML.load(concept[:concept_config]['text']) %>
+          <li>
+            <div class='title'>
+              <a href="http://<%= concept[:concept_url] %>">
+                <h2><%= concept_yaml['name'] %></h2>
+              </a>
+            <div>
+            <div class='description'>
+              <%= concept_yaml['description'] %>
+            </div>
+            <div class='repo'>
+              <a href="<%= "https://github.com/#{concept[:login]}/#{concept[:repo]}" %>">
+                github
+              </a>
+            </div>
+          </li>
+        <% end %>
+      </ul>
+    </section>
   </body>
 </html>
